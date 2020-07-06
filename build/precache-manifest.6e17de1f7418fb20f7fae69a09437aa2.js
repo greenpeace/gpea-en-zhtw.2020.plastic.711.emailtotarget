68be3677e51785088d6f8b9d088d4ea73483483b
garrychiao <g50905g@gmail.com>
2020-07-06 11:38:35 +0800

    safari form error bug fixed

---
+++ build/precache-manifest.6e17de1f7418fb20f7fae69a09437aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f9edff4d980f1f09e975fcbdaa1f02",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/index.html"
+  },
+  {
+    "revision": "f912daaf34c135586244",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/static/css/main.b156d55f.chunk.css"
+  },
+  {
+    "revision": "fd77c2ce46197505a62a",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/static/js/2.0279fee2.chunk.js"
+  },
+  {
+    "revision": "299e0fcb38a3775f9dc1746e56b90d3c",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/static/js/2.0279fee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f912daaf34c135586244",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/static/js/main.2b6b1dd8.chunk.js"
+  },
+  {
+    "revision": "e8f6b9ad9a6d9a740a69",
+    "url": "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/static/js/runtime-main.3ef38710.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/precache-manifest.54a9c40c799ad5d92fe539281b18d970.js"
+  "https://change.greenpeace.org.tw/2020/petition/zh-TW.2020-plastic-retailer-seveneleven.signup.dd/precache-manifest.6e17de1f7418fb20f7fae69a09437aa2.js"
 );
 
 self.addEventListener('message', (event) => {
